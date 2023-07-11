4975a4acffc5919361fc9ad73281ba1c63279a98
AmanSoni1 <amansoni0908@gmail.com>
2023-07-11 15:08:18 +0530

    Complete

---
+++ src/assets/index.js
@@ -0,0 +1,62 @@
+import logo from "../../log1.jpg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/mysql.png";
+import docker from "./tech/da.png";
+import figma from "./tech/r.png";
+import git from "./tech/git.png";
+import html from "./tech/python.png";
+import javascript from "./tech/powerbi.png";
+import mongodb from "./tech/pandas.png";
+import nodejs from "./tech/scikit.png";
+import reactjs from "./tech/tensorflow.png";
+import redux from "./tech/ml.png";
+import tailwind from "./tech/numpy.png";
+import typescript from "./tech/excel.png";
+import threejs from "./tech/seaborn.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/pwc_logo.png";
+import tesla from "./company/KPMG_logo.png";
+
+import carrent from "./Quora.png";
+import jobit from "./mr.png";
+import tripguide from "./KM.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
---
+++ src/components/About.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        style={{textAlign : "justify"}}
+      >
+       I am a recent graduate from IIT Dhanbad with an MSc in Mathematics and Computing, My projects in machine 
+       learning and PowerBI have equipped me with practical skills.
+       Also with a proficiency in MySQL, I possess the necessary skills to work with databases and perform data extraction, 
+       transformation, and loading tasks. Furthermore, my mathematical background enhances my statistical understanding, 
+       enabling me to derive meaningful insights from data and make data-driven decisions.<br/><br/>As a fresher, I am eager to 
+       apply my skills and contribute to a dynamic and challenging environment. I am seeking opportunities where I can 
+       leverage my expertise in data analysis, machine learning, statistical understanding, and web development to drive 
+       data-informed decision-making and deliver valuable insights.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
